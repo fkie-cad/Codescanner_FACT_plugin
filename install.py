5164c0131435b3788bda7d02d7089998a0b56e51
Jörg Stucke <joerg.stucke@fkie.fraunhofer.de>
2022-05-11 11:44:36 +0200

    switched to pip installation

--- install.py
+++ install.py
@@ -1,34 +1,22 @@
 #!/usr/bin/env python3
-
+# pylint: disable=ungrouped-imports
 import logging
 from pathlib import Path
 
 try:
-    from helperFunctions.install import OperateInDirectory, is_virtualenv, run_cmd_with_logging, check_distribution
+    from helperFunctions.install import check_distribution
     from plugins.installer import AbstractPluginInstaller
 except ImportError:
     import sys
     SRC_PATH = Path(__file__).absolute().parent.parent.parent.parent
     sys.path.append(str(SRC_PATH))
 
-    from helperFunctions.install import OperateInDirectory, is_virtualenv, run_cmd_with_logging, check_distribution
+    from helperFunctions.install import check_distribution
     from plugins.installer import AbstractPluginInstaller
 
 
 class CodescannerInstaller(AbstractPluginInstaller):
     base_path = Path(__file__).resolve().parent
-
-    def build(self):
-        build_dir = self.base_path / 'build' / 'Codescanner'
-        run_cmd_with_logging(
-            f'git clone https://github.com/fkie-cad/Codescanner.git -o Codescanner', raise_error=True
-        )
-        with OperateInDirectory(build_dir, remove=True):
-            command = 'python3 setup.py install'
-            if not is_virtualenv():
-                logging.warning("no venv")
-                command = f'sudo -EH {command}'
-            run_cmd_with_logging(command)
 
 
 # Alias for generic use
@@ -36,6 +24,4 @@ Installer = CodescannerInstaller
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    distribution = check_distribution()
-    installer = Installer(distribution)
-    installer.install()
+    Installer(check_distribution()).install()
